c47bf94bb9fe1c4c11ac1b681e6124b861123ab5
rebornchris <415817317@qq.com>
2017-09-07 17:31:00 +0800

    import menu

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -2,22 +2,38 @@
 <div id="wrapper">
   <mu-appbar title="456Wings">
     <mu-text-field icon="search" class="appbar-search-field" slot="right" hintText="search for sth..." />
-    <mu-flat-button color="white" label="Setting" slot="right" />
+    <mu-flat-button color="white" icon='settings' label="Settings" slot="right" ref="button" @click="toggle" />
+    <mu-popover :trigger="trigger" slot='right' :open="open" @close="handleClose">
+      <mu-menu>
+        <mu-menu-item title="Refresh" />
+        <mu-menu-item title="Send feedback" />
+        <mu-menu-item title="Settings" />
+        <mu-menu-item title="Help" />
+        <mu-menu-item title="Sign out" />
+      </mu-menu>
+    </mu-popover>
   </mu-appbar>
 </div>
 </template>
 
 <script>
-import SystemInformation from './LandingPage/SystemInformation'
-
 export default {
-  name: 'landing-page',
-  components: {
-    SystemInformation
+  data() {
+    return {
+      value: '3',
+      open: false,
+      trigger: null
+    }
+  },
+  mounted() {
+    this.trigger = this.$refs.button.$el
   },
   methods: {
-    open(link) {
-      this.$electron.shell.openExternal(link)
+    toggle() {
+      this.open = !this.open
+    },
+    handleClose() {
+      this.open = false
     }
   }
 }
@@ -34,16 +50,31 @@ export default {
 body {
   font-family: 'Source Sans Pro', sans-serif;
 }
-.appbar-search-field{
+
+.appbar-search-field {
   margin-top: 12px;
 }
+
 .mu-text-field-hint {
   color: fade(#FFF, 54%);
 }
+
 .mu-text-field-input {
   color: #FFF;
 }
+
 .mu-text-field-focus-line {
   background-color: #FFF;
+}
+
+.menu-list {
+  position: absolute;
+  right: 0;
+  top: 64px;
+  width: 224px;
+}
+
+.demo-menu {
+  width: 224px;
 }
 </style>
